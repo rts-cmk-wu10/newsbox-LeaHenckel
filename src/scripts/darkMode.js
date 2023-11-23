c2f410913c62dcd549c7e93183336603bb27c681
Lea Henckel <lea.henckel@outlook.com>
2023-11-23 14:00:22 +0100

    done result

--- src/scripts/darkMode.js
+++ src/scripts/darkMode.js
@@ -1,11 +1,31 @@
 export default (function () {
-  if (!window.location.pathname.includes("settings.html")) return; // guarde clause
-
   const CTA_BUTTON = document.querySelector(".activate__darkMode--btn");
+
+  // Check if the dark mode is enabled in the local storage and apply it
+  if (localStorage.getItem("theme") === "darkmode") {
+    enableDarkMode();
+  }
 
   CTA_BUTTON.addEventListener("click", clickHandler);
 
   function clickHandler() {
-    document.body.classList.toggle("darkmode");
+    const CLASS_LIST = document.body.classList;
+    CLASS_LIST.toggle("darkmode");
+
+    // Update the theme in local storage based on the current state
+    localStorage.setItem(
+      "theme",
+      CLASS_LIST.contains("darkmode") ? "darkmode" : ""
+    );
+  }
+
+  function enableDarkMode() {
+    const CLASS_LIST = document.body.classList;
+    CLASS_LIST.add("darkmode");
+  }
+
+  // Check if the "theme" is set in the local storage, and if not, initialize it
+  if (!localStorage.getItem("theme")) {
+    localStorage.setItem("theme", "");
   }
 })();
--- src/scripts/toggleCategory.js
+++ src/scripts/toggleCategory.js
@@ -3,13 +3,21 @@ export default (function () {
 
   const checkboxes = document.querySelectorAll("input[type=checkbox]");
 
+  // Retrieve existing data from local storage
+  const existingData = getJSONfromLocalStorage("checkboxData");
+
+  // Initialize checkboxes on the settings page by setting its checked state based on whether there is a saved state for its category in the existingData retrieved from local storage.
+  // If there is no saved state, the default is set to true. It also adds an event listener to each checkbox to respond to changes, ensuring that the toggleHandler function is called when a user interacts with the checkbox.
   checkboxes.forEach(function (element) {
+    const category = element.dataset.category;
+    element.checked =
+      existingData[category] !== undefined ? existingData[category] : true;
     element.addEventListener("change", toggleHandler);
   });
 
   function toggleHandler(event) {
-    const isChecked = event.target.checked;
-    const category = event.target.dataset.category;
+    const isChecked = event.currentTarget.checked;
+    const category = event.currentTarget.dataset.category;
 
     // Save data to local storage
     saveToLocalStorage(category, isChecked);
@@ -17,12 +25,20 @@ export default (function () {
 
   function saveToLocalStorage(key, value) {
     // Retrieve existing data from local storage
-    const existingData = JSON.parse(localStorage.getItem("checkboxData")) || {};
+    const existingData = getJSONfromLocalStorage("checkboxData");
 
     // Update or add the new data
     existingData[key] = value;
 
     // Save the updated data back to local storage
     localStorage.setItem("checkboxData", JSON.stringify(existingData));
+  }
+
+  // Retrieves and parses JSON data stored in the local storage of a web browser.
+  function getJSONfromLocalStorage(key) {
+    const OBJECT = localStorage.getItem(key)
+      ? JSON.parse(localStorage.getItem(key))
+      : [];
+    return OBJECT;
   }
 })();
